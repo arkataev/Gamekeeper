4fd34cc8506048dec890d1eb0746279670b1417b
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-12-17 20:23:57 +0000

    procfile added

---
+++ Procfile
@@ -0,0 +1,3 @@
+bot: python3 gamekeeper
+ps:scale web=0
+ps:scale bot=1
--- gamekeeper/__init__.py
+++ gamekeeper/__init__.py
@@ -5,16 +5,4 @@ from gamekeeper.bot import bot
 def bot_msg_handler(msg):
     bot.send(msg.text, msg.chat['id'])
 
-if __name__ == '__main__':
-    bot.run(bot_msg_handler)
-
-    # {'message': {
-    #     'from': {'id': 54159458, 'first_name': 'Александр', 'username': 'arkataev'},
-    #     'message_id': 110,
-    #     'entities': [{'length': 6, 'type': 'bot_command', 'offset': 0}],
-    #     'chat': {'id': 54159458, 'first_name': 'Александр', 'type': 'private', 'username': 'arkataev'},
-    #     'text': '/greet',
-    #     'date': 1481975069
-    # },
-    #     'update_id': 827649449
-    # }
+bot.run(bot_msg_handler)
