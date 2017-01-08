85cbb164d3a64bd8afc481f4322a50ff48dc42e1
reddev <alex@reddev.ru>
2017-01-08 13:54:48 +0300

    refactor_05

--- __main__.py
+++ __main__.py
@@ -1,9 +1,13 @@
 from gamekeeper import Gamekeeper, default_message_handler
 import re
 
+
 if __name__ == '__main__':
-    with open('.env') as env_file:
-        env = env_file.read()
-        params = re.findall(r"(?P<name>.*?)=(?P<value>.*)", env)
+    try:
+        with open('.env') as env_file:
+            env = env_file.read()
+            params = re.findall(r"(?P<name>.*?)=(?P<value>.*)", env)
+    except FileNotFoundError:
+        exit(u'.env not found')
     for param in params: setattr(Gamekeeper, param[0], param[1])
     Gamekeeper.run(default_message_handler)
